c9cf40e110fc3553e9030d904144838cd1bfd148
omolade <tosin.ogunmola44@gmail.com>
2023-04-19 22:33:46 +0100

    first commit

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,36 +1,36 @@
-import {useState} from 'react'
-
+import { useState } from "react";
 
 const Profile = (props) => {
-    const {userData} = props;
-    const [hovered, setHovered] = useState(false)
+  const { userData } = props;
+  const [hovered, setHovered] = useState(false);
 
-    const handleMouseEnter = () => {
-        setHovered(true)
-    }
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
 
-    const handleMouseLeave = () => {
-        setHovered(false)
-    }
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
   return (
-    <div 
-    className='movie-info'
-    onMouseEnter={handleMouseEnter}
-    onMouseLeave={handleMouseLeave}
-    style={{ transform: hovered ? 'scale(1.05)' : 'scale(1)'}}
+    <div
+      className="movie-info"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={{
+        transform: hovered ? "scale(1.05)" : "scale(1)",
+        transition: 0.5,
+        boxShadow: hovered ? "5px 5px 5px #aaaaaa" : "1px 1px 1px ",
+      }}
     >
-        
-      <h1 className='piece'> {userData.title} </h1>
+      <h1 className="piece"> {userData.title} </h1>
       <h2> {userData.release_date} </h2>
-      <p className='before'> {userData.opening_crawl} </p>
-      <div className='bord'>
-      </div>
-     <a href="" className='link'>More Link</a>
-
-      
+      <p className="before"> {userData.opening_crawl} </p>
+      <div className="bord"></div>
+      <a href="" className="link">
+        More Link
+      </a>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
